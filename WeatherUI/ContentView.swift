e6bb551a2df7fc6a014fd8719797f5853b7ba903
zee-RGB <zaahir85@gmail.com>
2024-12-07 03:39:34 +1000

    Initial Commit

---
+++ WeatherUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherUI
+//
+//  Created by zee-RGB on 7/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherUI/WeatherUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherUIApp.swift
+//  WeatherUI
+//
+//  Created by zee-RGB on 7/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
